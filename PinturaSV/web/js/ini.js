55aedbc37f6598067b6ff22b2b91f4255654bcf4
AndresK21 <andresdosmil@gmail.com>
2018-02-04 17:58:20 -0600

    Ingreso del proyecto

---
+++ PinturaSV/web/js/ini.js
@@ -0,0 +1,8 @@
+$( document ).ready(function(){
+    $(".button-collapse").sideNav();
+    $('.slider').slider();
+    $(".dropdown-button").dropdown();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('#modal12').modal('open');
+})
