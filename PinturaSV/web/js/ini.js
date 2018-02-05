b453fec8f878d283cbe3c596e85382aef3a0518f
Diego Guzman <36141469+DGuzman23@users.noreply.github.com>
2018-02-04 19:58:55 -0600

    Eliminar

--- PinturaSV/web/js/ini.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function(){
-    $(".button-collapse").sideNav();
-    $('.slider').slider();
-    $('.parallax').parallax();
-    $('.modal').modal();
-    $('ul.tabs').tabs('select_tab', 'tab_id');
-    $('ul.tabs').tabs();
-    $('select').material_select();
-})
