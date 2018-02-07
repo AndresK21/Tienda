76b33845b1ece883484942d962326de7f30f69e2
Diego Guzman <36141469+DGuzman23@users.noreply.github.com>
2018-02-07 17:00:42 -0600

    Error 404

---
+++ PinturaSV/controllers/view/ini_sesion.php
@@ -0,0 +1,21 @@
+<!-- Modal de iniciar sesion -->
+<div id="modal_inicio" class="modal ">
+    <div class="modal-content">
+        <h4>Iniciar sesi&oacute;n</h4>
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="usuario" type="text" class="validate">
+                    <label for="usuario">Nombre de usuario</label>
+                </div>
+            </div>
+            <div class="row">
+            <div class="input-field col s12">
+                <input id="contrasena" type="text" class="validate">
+                <label for="contrasena">Contraseña</label>
+            </div>
+    </div>
+    <div class="modal-footer">
+        <a href="#!" class="modal-action modal-close waves-effect waves-red btn-flat ">Cancelar</a>
+        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat ">Iniciar sesi&oacute;n</a>
+    </div>
+</div>
