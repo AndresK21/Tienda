b0a1e0dc2031dc93eda88ba4bfe9fec1043faad6
andresdosmil@gmail.com <makina8950>
2018-07-24 11:57:08 -0600

    SV

--- pinturasv/app/controllers/dashboard/empleados/correo_controller.php
+++ pinturasv/app/controllers/dashboard/empleados/correo_controller.php
@@ -1,47 +1,23 @@
 <?php
 require_once("../../app/models/empleado.class.php");
-require_once("../../app/helpers/empleado.class.php");
-require_once("../../app/models/empleado.class.php");
+require_once("../../app/helpers/PHPMailer.class.php");
+require_once("../../app/helpers/Exception.class.php");
 try{
-    $empleado = new Empleado;
-    if($empleado->getEmpleados()){
-        Page::showMessage(3, "Hay usuarios disponibles", "login.php");
-    }else{
-        if(isset($_POST['crear'])){
-        $_POST = $empleado->validateForm($_POST);
-        if($empleado->setNombre($_POST['nombre_completo'])){
-            if($empleado->setCorreo($_POST['correo_electronico'])){
-                if($empleado->setUsuario($_POST['nombre_usuario'])){
-                    if($empleado->setContrasena($_POST['contrasena'])){
-                        if($empleado->setId_permiso("2")){ //Establece que solo puede ser administrador
-                            if(is_uploaded_file($_FILES['archivo']['tmp_name'])){
-                                if($empleado->setImagen($_FILES['archivo'])){
-                                    if($empleado->createEmpleado()){
-                                        Page::showMessage(1, "Usuario creado", "login.php");
-                                    }else{
-                                        throw new Exception(Database::getException());
-                                    }
-                                }else{
-                                    throw new Exception($producto->getImageError());
-                                }
-                            }else{
-                                throw new Exception("Seleccione una imagen");
-                            }
-                        }else{
-                            throw new Exception("Elige un permiso");
-                        }                  
-                    }else{
-                        throw new Exception("Contraseña incorrecta");
-                    }
-                }else{
-                    throw new Exception("Usuario incorrecto");
-                }
+    if(isset($_POST['enviar'])){
+    $_POST = $empleado->validateForm($_POST);
+    if($nombre = validateAlphabetic($_POST['nombre'], 20, 150)){
+        if($correo = validateEmail($_POST['correo'])){
+            if($mensaje = validateAlphanumeric($_POST['mensaje'], 80, 500)){
+
+                
             }else{
-                throw new Exception("Correo incorrecto");
+                throw new Exception("Mensaje incorrecto");
             }
         }else{
-            throw new Exception("Nombres incorrectos");
+            throw new Exception("Correo incorrecto");
         }
+    }else{
+        throw new Exception("Nombre incorrecto");
     }
 }
 }catch(Exception $error){
--- pinturasv/app/view/dashboard/empleado/correo_view.php
+++ pinturasv/app/view/dashboard/empleado/correo_view.php
@@ -13,12 +13,14 @@
             <label for="cmail" class="black-text">Correo electr&oacute;nico</label>
         </div>
         <div class="input-field col s12">
-            <input id="tel" type="number" name="telefono" min="70000000" step="any" class="validate" required>
-            <label for="tel" class="black-text">Tel&eacute;fono</label>
-        </div>
-        <div class="input-field col s12">
             <textarea id="mensaj" type="text" name="mensaje" class="materialize-textarea" required></textarea>
             <label for="mensaj" class="black-text">Mensaje</label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 right-align">
+            <a class='btn waves-effect red darken-3' href="index.php"><i class='material-icons'></i>Cancelar</a>
+            <button type='submit' name='enviar' class='btn waves-effect blue-grey darken-4'><i class='material-icons'>mail</i>Enviar</button>
         </div>
     </div>
 <form>
